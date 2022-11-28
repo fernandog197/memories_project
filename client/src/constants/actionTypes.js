98b7e4be6c1ed25c533204870a13f8dac8620b0a
fernandog197 <bluewavesalta@gmail.com>
2022-11-28 20:29:37 -0300

    memories-project_v1.0.0

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,13 @@
+const FETCH_ALL = 'FETCH_ALL'
+const CREATE = 'CREATE'
+const UPDATE = 'UPDATE'
+const DELETE = 'DELETE'
+const LIKE = 'LIKE'
+
+export {
+    FETCH_ALL,
+    CREATE,
+    UPDATE,
+    DELETE,
+    LIKE
+}
